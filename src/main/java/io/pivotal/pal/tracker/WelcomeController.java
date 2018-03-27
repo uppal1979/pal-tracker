956f689b6ca6a62388a9683ecf9db3f7a4da02a8
potlamf <potlamf@gmail.com>
2018-03-27 16:02:56 +0800

    adding missing files

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    String welcomestring;
+
+   public WelcomeController(@Value("${WELCOME_MESSAGE}")String inputString){
+        this.welcomestring = inputString;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return welcomestring;
     }
 }
